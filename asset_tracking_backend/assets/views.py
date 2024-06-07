549cef12a6e38260b540cf661f1980eb1e7f4763
visteen <visteen192@gmail.com>
2024-06-07 07:38:59 +0000

    minor changes

--- asset_tracking_backend/assets/views.py
+++ asset_tracking_backend/assets/views.py
@@ -48,33 +48,6 @@ class TagsAPI(SimpleCrudMixin):
     serializer_class = GettAllTagSerializer
     model_class = Tag
             
-# class DeleteTagAPI(generics.GenericAPIView):
-#     permission_classes = [permissions.IsAuthenticated,APILevelPermissionCheck]
-#     required_permissions = [ "setup.delete_tag"]
-#     serializer_class = DeleteTagSerializer
-#     def delete(self, request, *args, **kwargs):
-#         serializers = self.serializer_class(data=request.data)
-#         if serializers.is_valid():
-#             id = serializers.data['tag_id']
-#             try:
-#                 tag = Tag.objects.get(id=id)
-#                 tag.delete()
-#                 return Response(
-#                     {
-#                         "status": "success",
-#                         "detail": "Tag Deleted Successfully",
-#                     },
-#                     status=status.HTTP_200_OK,
-#                 )
-#             except Tag.DoesNotExist:
-#                 return Response(
-#                     {"status": "error", "detail":"Tag Not Found"},
-#                     status=404
-#                 )
-#         return Response(
-#             {"status": "failure", "detail": serializers.errors},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#         )
             
             
 class AddProductAPI(SimpleCrudMixin):
@@ -120,35 +93,7 @@ class ProductsAPI(SimpleCrudMixin):
     serializer_class = GettAllProductSerializer
     model_class = Product
         
-# class DeleteProductAPI(generics.GenericAPIView):
-#     permission_classes = [permissions.IsAuthenticated,APILevelPermissionCheck]
-#     required_permissions = [ "setup.delete_product"]
-    
-#     serializer_class = DeleteProductSerializer
-#     def delete(self, request, *args, **kwargs):
-#         serializers = self.serializer_class(data=request.data)
-#         if serializers.is_valid():
-#             id = serializers.data['product_id']
-#             try:
-#                 product = Product.objects.get(id=id)
-#                 product.delete()
-#                 return Response(
-#                     {
-#                         "status": "success",
-#                         "detail": "Product Deleted Successfully",
-#                     },
-#                     status=status.HTTP_200_OK,
-#                 )
-#             except Product.DoesNotExist:
-#                 return Response(
-#                     {"status": "error", "detail":"Product Not Found"},
-#                     status=404
-#                 )
-#         return Response(
-#             {"status": "failure", "detail": serializers.errors},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#         )
-        
+  
 class AddAssetAPI(generics.GenericAPIView):
     """ check for require permission for adding a asset """
     permission_classes = [permissions.IsAuthenticated,APILevelPermissionCheck]
@@ -193,31 +138,3 @@ class GetAllAsssetsAPI(generics.GenericAPIView):
             status=200
         )   
         
-class DeleteAssetAPI(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated,APILevelPermissionCheck]
-    required_permissions = [ "setup.delete_asset"]
-    
-    serializer_class = DeleteAssetSerializer
-    def delete(self, request, *args, **kwargs):
-        serializers = self.serializer_class(data=request.data)
-        if serializers.is_valid():
-            id = serializers.data['asset_id']
-            try:
-                asset = Asset.objects.get(id=id)
-                asset.delete()
-                return Response(
-                    {
-                        "status": "success",
-                        "detail": "Asset Deleted Successfully",
-                    },
-                    status=status.HTTP_200_OK,
-                )
-            except Asset.DoesNotExist:
-                return Response(
-                    {"status": "error", "detail":"Asset Not Found"},
-                    status=404
-                )
-        return Response(
-            {"status": "failure", "detail": serializers.errors},
-                status=status.HTTP_400_BAD_REQUEST,
-        )
--- asset_tracking_backend/users/views.py
+++ asset_tracking_backend/users/views.py
@@ -194,34 +194,34 @@ class UsersAPI(SimpleCrudMixin):
     model_class = User
     
 
-class DeleteInstitutionAPI(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated,APILevelPermissionCheck]
-    required_permissions = [ "setup.delete_institution"]
+# class DeleteInstitutionAPI(generics.GenericAPIView):
+#     permission_classes = [permissions.IsAuthenticated,APILevelPermissionCheck]
+#     required_permissions = [ "setup.delete_institution"]
     
-    serializer_class = DeleteInstitutionSerializer
-    def delete(self, request, *args, **kwargs):
-        serializers = self.serializer_class(data=request.data)
-        if serializers.is_valid():
-            id = serializers.data['institution_id']
-            try:
-                institution = Institution.objects.get(id=id)
-                institution.delete()
-                return Response(
-                    {
-                        "status": "success",
-                        "detail": "Institution Deleted Successfully",
-                    },
-                    status=status.HTTP_200_OK,
-                )
-            except Institution.DoesNotExist:
-                return Response(
-                    {"status": "error", "detail":"Institution Not Found"},
-                    status=404
-                )
-        return Response(
-            {"status": "failure", "detail": serializers.errors},
-                status=status.HTTP_400_BAD_REQUEST,
-        )
+#     serializer_class = DeleteInstitutionSerializer
+#     def delete(self, request, *args, **kwargs):
+#         serializers = self.serializer_class(data=request.data)
+#         if serializers.is_valid():
+#             id = serializers.data['institution_id']
+#             try:
+#                 institution = Institution.objects.get(id=id)
+#                 institution.delete()
+#                 return Response(
+#                     {
+#                         "status": "success",
+#                         "detail": "Institution Deleted Successfully",
+#                     },
+#                     status=status.HTTP_200_OK,
+#                 )
+#             except Institution.DoesNotExist:
+#                 return Response(
+#                     {"status": "error", "detail":"Institution Not Found"},
+#                     status=404
+#                 )
+#         return Response(
+#             {"status": "failure", "detail": serializers.errors},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#         )
         
 class LogOutAPI(generics.GenericAPIView):
     """Logout API view to blacklist refresh token"""
