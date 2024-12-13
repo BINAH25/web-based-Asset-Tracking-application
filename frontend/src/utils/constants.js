20eaddcf4dc9c4e4a1e4566bf89f2db48c21ef10
BINAH25 <visteen192@gmail.com>
2024-12-13 15:13:04 +0000

    dockerfile added

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_API_URI = 'http://127.0.0.1:8000';
+export const BASE_API_URI = process.env.VITE_BACKEND_URL || 'http://127.0.0.1:8000';
 //export const BASE_API_URI = 'https://dcit418.pythonanywhere.com';
