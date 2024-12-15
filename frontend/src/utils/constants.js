a964a2ff90c81e56893a4e5023eab5652a4186f0
BINAH25 <visteen192@gmail.com>
2024-12-15 09:50:11 +0000

    nginx conf added

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,1 @@
-export const BASE_API_URI = process.env.VITE_BACKEND_URL || 'http://12.0.2.88:8000';
-//export const BASE_API_URI = 'https://dcit418.pythonanywhere.com';
+export const BASE_API_URI = '/api/';
